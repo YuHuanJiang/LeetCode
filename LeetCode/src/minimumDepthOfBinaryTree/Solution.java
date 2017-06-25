2a4e4668f884798c7241411badcdcb80dbe8c0a8
YuHuanJiang <bb812340176@vip.qq.com>
2017-06-25 16:46:09 +0800

    Minimum Depth Of Binary Tree

---
+++ LeetCode/src/minimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,15 @@
+package minimumDepthOfBinaryTree;
+
+import entity.TreeNode;
+
+public class Solution {
+	public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        
+        if(root.left == null && root.right == null) return 1;
+        
+        if(root.left == null) return minDepth(root.right) + 1;
+        else if(root.right == null) return minDepth(root.right) + 1;
+        else return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
