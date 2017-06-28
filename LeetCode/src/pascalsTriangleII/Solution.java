3f95f4910884f52302def67d5762a6f5aca227bc
YuHuanJiang <bb812340176@vip.qq.com>
2017-06-28 22:11:01 +0800

    Pascal's Triangle II

---
+++ LeetCode/src/pascalsTriangleII/Solution.java
@@ -0,0 +1,20 @@
+package pascalsTriangleII;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        
+        if(rowIndex <= 0) return result;
+        
+        result.add(1);
+        for(int i = 1; i <= rowIndex; i++) {
+        	for(int j = result.size() - 2; j >= 0; j--) result.set(j+1,result.get(j)+result.get(j+1));
+        	result.add(1); 
+        }
+        
+        return result;
+    }
+}
