92e6f88c027a41a68b7cb9dd5b935e0e5bb4b32e
YuHuanJiang <bb812340176@vip.qq.com>
2017-07-09 20:50:02 +0800

    Single Number II

---
+++ LeetCode/src/singleNumberII/Solution.java
@@ -0,0 +1,17 @@
+package singleNumberII;
+
+public class Solution {
+	public int singleNumber(int[] nums) {
+		int ones  = 0, twos = 0, threes = 0;
+		
+		for(int i = 0; i < nums.length; i++) {
+			twos |= ones & nums[i];
+			ones ^= nums[i];
+			threes = ones & twos;
+			ones &= ~threes;
+			twos &= ~threes;
+		}
+		
+		return ones;
+    }
+}
