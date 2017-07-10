db93c3e5e09f305bc1f593d50bde9ff55773cb62
YuHuanJiang <bb812340176@vip.qq.com>
2017-07-10 19:10:40 +0800

    Linked List Cycle

---
+++ LeetCode/src/linkedListCycle/Solution.java
@@ -0,0 +1,19 @@
+package linkedListCycle;
+
+import entity.ListNode;
+
+public class Solution {
+	public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+        	slow = slow.next;
+        	fast = fast.next.next;
+        	
+        	if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+}
